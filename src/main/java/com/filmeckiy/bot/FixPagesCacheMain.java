6bd567ef6bf8716d7123f15e1e49f82ccef4c618
Egor Lifar <egor.lifar@gmail.com>
2015-08-12 17:45:11 +0300

    experiment

--- src/main/java/com/filmeckiy/bot/FixPagesCacheMain.java
+++ src/main/java/com/filmeckiy/bot/FixPagesCacheMain.java
@@ -3,6 +3,7 @@ package com.filmeckiy.bot;
 import com.filmeckiy.bot.kp.KpClient;
 import com.filmeckiy.bot.kp.MongoUtils;
 import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bson.Document;
@@ -16,14 +17,25 @@ public class FixPagesCacheMain {
     private static final Logger logger = LogManager.getLogger(FixPagesCacheMain.class);
 
     public static void main(String[] args) {
-        KpClient kpClient = new KpClient();
 
-        FindIterable<Document> iter = MongoUtils.getClient()
+        MongoCollection<Document> pagesCacheCollection = MongoUtils.getClient()
                 .getDatabase(MongoUtils.DB_NAME)
-                .getCollection("pages_cache")
-                .find();
+                .getCollection("pages_cache");
 
-        for (Document doc : iter) {
+        for (Document doc : pagesCacheCollection.find()) {
+            int length = doc.getString("text").length();
+
+            if (length < 500) {
+                String url = doc.getString("_id");
+                logger.info("Found small text: {} {}", url, length);
+
+                pagesCacheCollection.deleteOne(new Document("_id", url));
+            }
+        }
+
+        KpClient kpClient = new KpClient();
+        for (Document doc : pagesCacheCollection
+                .find()) {
             List<String> urls = kpClient.getBfsUrls(doc.getString("_id"));
             urls.forEach(kpClient::addToQueue);
         }
