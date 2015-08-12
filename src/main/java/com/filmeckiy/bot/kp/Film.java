51b309d0177d5905bda5b565d92ffa133bfa0a7b
Egor Lifar <egor.lifar@gmail.com>
2015-08-12 17:16:55 +0300

    test tor

---
+++ src/main/java/com/filmeckiy/bot/kp/Film.java
@@ -0,0 +1,12 @@
+package com.filmeckiy.bot.kp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author egor
+ */
+public class Film {
+    private static final Logger logger = LogManager.getLogger(Film.class);
+
+}
