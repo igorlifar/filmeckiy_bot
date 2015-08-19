1de84c41632303242cae8b17cc78be6a7f225a2d
Egor Lifar <egor.lifar@gmail.com>
2015-08-19 19:57:01 +0300

    debug

--- src/main/java/com/filmeckiy/bot/Main.java
+++ src/main/java/com/filmeckiy/bot/Main.java
@@ -249,7 +249,7 @@ public class Main {
         double ans = film.kpRating.getOrElse(0.);
 
         ans += 100 * equals(queryTokens, StringUtils.main(film.title));
-        ans += 150 * equals(queryTokens, StringUtils.main(film.year.getOrElse("")));
+        ans += 250 * equals(queryTokens, StringUtils.main(film.year.getOrElse("")));
         ans += 50 * equals(queryTokens, StringUtils.main(film.director.getOrElse("")));
         ans += 10 * equals(queryTokens, StringUtils.main(film.slogan.getOrElse("")));
 
